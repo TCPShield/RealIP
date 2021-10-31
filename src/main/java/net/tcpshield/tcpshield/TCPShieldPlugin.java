cf97e376323f5fcdd0f2113d11007922dbb8d024
Dylan Keir <me@dyl4n.me>
2021-10-31 15:43:08 +0000

    Add try/catch to java version check

--- src/main/java/net/tcpshield/tcpshield/TCPShieldPlugin.java
+++ src/main/java/net/tcpshield/tcpshield/TCPShieldPlugin.java
@@ -12,24 +12,28 @@ public interface TCPShieldPlugin {
 
 	/**
 	 * Gets the plugin's config provider
+	 *
 	 * @return The plugin's config provider
 	 */
 	ConfigProvider getConfigProvider();
 
 	/**
 	 * Gets the plugin's logger
+	 *
 	 * @return The plugin's logger
 	 */
 	Logger getLogger();
 
 	/**
 	 * Gets the packet handler
+	 *
 	 * @return The packet handler
 	 */
 	TCPShieldPacketHandler getPacketHandler();
 
 	/**
 	 * Gets the plugin's debugger
+	 *
 	 * @return The plugin's debugger
 	 */
 	Debugger getDebugger();
@@ -40,12 +44,15 @@ public interface TCPShieldPlugin {
 	 */
 	default void initialization() {
 		String jvmVersion = System.getProperty("java.version");
-
-		// Java 11 check/warning (Will eventually force Java 16 without warning)
-		String[] versionParts = jvmVersion.split("\\.");
-		int baseVersion = Integer.parseInt(versionParts[0]);
-		if (baseVersion < 11) // Java 8, and below, starts with 1, but since we are using Java 11 we can ignore sub values
-			this.getDebugger().warn("The Java version you are running is outdated for TCPShield and may cause issues. Update to atleast Java 11. Your version: Java %s", jvmVersion);
+		try {
+			// Java 11 check/warning (Will eventually force Java 16 without warning)
+			String[] versionParts = jvmVersion.split("\\.");
+			int baseVersion = Integer.parseInt(versionParts[0]);
+			if (baseVersion < 11) // Java 8, and below, starts with 1, but since we are using Java 11 we can ignore sub values
+				this.getDebugger().warn("The Java version you are running is outdated for TCPShield and may cause issues. Update to atleast Java 11. Your version: Java %s", jvmVersion);
+		} catch (Throwable t) {
+			this.getDebugger().error("Failed to check java version for string: " + jvmVersion);
+		}
 	}
 
 }
