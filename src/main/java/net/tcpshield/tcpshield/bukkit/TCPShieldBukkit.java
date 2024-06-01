554d6c300e531e5cfc386bf354f19b3d6835c749
Dylan Keir <contact@dylankeir.uk>
2024-06-01 05:29:39 +0100

    2.8.0 - Bump required Java verison from 8 -> 17, warn 1.20.5/1.20.6 users about incompatible ProtocolLib versions, also disable prefer-protocollib by default in favor of Paper handler

--- src/main/java/net/tcpshield/tcpshield/bukkit/TCPShieldBukkit.java
+++ src/main/java/net/tcpshield/tcpshield/bukkit/TCPShieldBukkit.java
@@ -1,5 +1,6 @@
 package net.tcpshield.tcpshield.bukkit;
 
+import java.util.logging.Logger;
 import net.tcpshield.tcpshield.TCPShieldPacketHandler;
 import net.tcpshield.tcpshield.TCPShieldPlugin;
 import net.tcpshield.tcpshield.bukkit.paper.BukkitPaper;
@@ -9,6 +10,7 @@ import net.tcpshield.tcpshield.geyser.GeyserUtils;
 import net.tcpshield.tcpshield.provider.ConfigProvider;
 import net.tcpshield.tcpshield.util.Debugger;
 import net.tcpshield.tcpshield.util.exception.phase.InitializationException;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -31,7 +33,25 @@ public class TCPShieldBukkit extends JavaPlugin implements TCPShieldPlugin {
 
 			// check force plib option -> paper -> plib -> error
 			if (this.configProvider.preferProtocolLib() && getServer().getPluginManager().getPlugin("ProtocolLib") != null) {
-				bukkitImpl = new BukkitProtocolLib(this);
+				try {
+					String[] protocolLibVersion = getServer().getPluginManager().getPlugin("ProtocolLib").getDescription().getVersion().split("-")[0].split("\\.");
+					int major = Integer.parseInt(protocolLibVersion[0]);
+					int minor = Integer.parseInt(protocolLibVersion[1]);
+					int patch = Integer.parseInt(protocolLibVersion[2]);
+
+					String paperVersion = Bukkit.getServer().getMinecraftVersion();
+					if (major <= 5 && minor <= 2 && patch <= 1 && (paperVersion.equals("1.20.5") || paperVersion.equals("1.20.6"))) {
+						getLogger().severe("TCPShield is incompatible with ProtocolLib <= 5.2.1 on Paper 1.20.5/1.20.6 due to lack of support from ProtocolLib. Reverting to default Paper handler to prevent issues. This error can be avoided by disabling 'prefer-protocollib' in the config.");
+						bukkitImpl = new BukkitPaper(this);
+					} else {
+						bukkitImpl = new BukkitProtocolLib(this);
+					}
+				} catch (Exception t) {
+					getLogger().warning("Failed to check Paper or ProtocolLib version. This is not a critical error unless you are running Paper 1.20.5/1.20.6 with ProtocolLib version 5.2.1 or below.");
+					getDebugger().exception(t);
+
+					bukkitImpl = new BukkitProtocolLib(this);
+				}
 			} else if (BukkitImplProvider.hasPaperEvent()) {
 				bukkitImpl = new BukkitPaper(this);
 			} else if (getServer().getPluginManager().getPlugin("ProtocolLib") != null) {
@@ -70,6 +90,11 @@ public class TCPShieldBukkit extends JavaPlugin implements TCPShieldPlugin {
 	@Override
 	public ConfigProvider getConfigProvider() {
 		return configProvider;
+	}
+
+	@Override
+	public Logger getLogger() {
+		return super.getLogger();
 	}
 
 }
